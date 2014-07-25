a466d2def3915422386b3263bbd1d12b37b7c2f4
yuxi <yuxi@localhost.localdomain>
2014-07-25 08:27:11 -0700

    TwoSum solved

---
+++ TwoSum/median.cxx
@@ -0,0 +1,8 @@
+// "Write a function that finds the median of a set of three numbers, also find the Big O. Can it be done with only 2 comparisons, or do you need 3?" 
+
+
+int median( int a, int b, int c ){
+	
+		
+	
+}
--- TwoSum/twosum_backup.cxx
+++ TwoSum/twosum_backup.cxx
@@ -26,6 +26,8 @@ int Partition( std::vector<mypair>& sqlist, int low, int high, bool& lowmoved, b
 	lowmoved = false;
 	highmoved = false;
 	
+	//compare sqlist[low].value, sqlist[high].value and sqlist[[(low+high)/2]].value, take
+	//the median to be pivot, then exchange sqlist[pivot] and sqlist[low]	
 	int mid = (low+high)/2;
 	int pivot = -1;
 	if(sqlist[low].value > sqlist[high].value){
@@ -48,7 +50,7 @@ int Partition( std::vector<mypair>& sqlist, int low, int high, bool& lowmoved, b
                 sqlist[pivot].pos = tmp_pos;
 	}
 
-	int pivot_value = sqlist[low].value;
+	int pivot_value = sqlist[low].value; //first element in range serve as pivot
 	int pivot_pos = sqlist[low].pos;
 	while( low < high ){
 		
@@ -56,6 +58,8 @@ int Partition( std::vector<mypair>& sqlist, int low, int high, bool& lowmoved, b
 			if(sqlist[high].value < sqlist[high-1].value && high > low+1)highmoved=true;
 			--high;
 		}
+		//if( low!=high ) highmoved = true;
+		//swap element [high] and pivot (already saved)
 		sqlist[low].pos = sqlist[high].pos;
 		sqlist[low].value = sqlist[high].value;
 		while(low < high && sqlist[low].value <= pivot_value){
@@ -66,6 +70,7 @@ int Partition( std::vector<mypair>& sqlist, int low, int high, bool& lowmoved, b
 		sqlist[high].value = sqlist[low].value;
 	}
 	
+	//fill in pivot 
 	sqlist[low].value = pivot_value;
 	sqlist[low].pos = pivot_pos;
 
@@ -80,9 +85,13 @@ void QSort( std::vector<mypair>& sqlist, int low, int high ){
 		bool lowmoved = false;
 		bool highmoved = false;
 		int pivotloc = Partition(sqlist, low, high, lowmoved, highmoved);
+	//	std::cout<<"pivotloc = "<<pivotloc<<", lowmoved = "<<lowmoved<<", highmoved = "<<highmoved<<std::endl;
 		if(lowmoved)QSort(sqlist,low,pivotloc-1);
 		if(highmoved)QSort(sqlist,pivotloc+1,high);
+	//	QSort(sqlist,low,pivotloc-1);
+	//	QSort(sqlist,pivotloc+1,high);
 	}
+	//else do nothing
 }
 
 void QuickSort( std::vector<mypair>& sqlist ){
@@ -119,19 +128,16 @@ int locateElement( std::vector<mypair>& sqlist, int low, int high, int target ){
                 if(target>sqlist[m].value){
                         low = m+1;
                 }
-                else if(target<sqlist[m].value){
+                else{
                         high = m-1;
                 }
-		else{
-			low = m;
-			high = m;
-		}
         }
 	if(target==sqlist[high].value) return high; 
 	else return -1;
 }
 
-std::vector<int> twoSum(std::vector<int> &numbers, int target) {
+//std::vector<int> twoSum(std::vector<int> &numbers, int target) {
+void twoSum(std::vector<int> &numbers, int target) {
 
 	std::vector<mypair> sqlist;
 	for(int i = 0; i < numbers.size(); i++){
@@ -142,23 +148,27 @@ std::vector<int> twoSum(std::vector<int> &numbers, int target) {
 	}
 	
 	QuickSort(sqlist);
+	
 	std::cout<<"after QuickSort()"<<std::endl;
-	for(int i = 0; i < sqlist.size(); i++)std::cout<<sqlist[i].value<<" , pos = "<<sqlist[i].pos+1<<std::endl;
+	for(int i = 0; i < sqlist.size(); i++)std::cout<<sqlist[i].value<<" , pos = "<<sqlist[i].pos+1<<std::endl;	
+	
 	
 	int low = 0;
 	int high = sqlist.size() - 1;
-//	int loc = locateInsert(sqlist,low,high,target);
-//	std::cout<<"loc = "<<loc<<std::endl;
+	int loc = locateInsert(sqlist,low,high,target);
+	std::cout<<"loc = "<<loc<<std::endl;
 	std::vector<int> idx(2);
 	bool found = false;
-        for(int it1 = 0; it1 < high; it1++){
+        for(int it1 = 0; it1 < loc; it1++){
                 int tid2 = target - sqlist[it1].value;
-		int id2loc = locateElement(sqlist, it1+1, high, tid2);		
-		std::cout<<"it1 = "<<it1<<std::endl;
-		std::cout<<"tid2 = "<<tid2<<std::endl;
-		std::cout<<"search range: "<<it1+1<<" to "<<high<<std::endl;
-		std::cout<<"id2loc = "<<id2loc<<std::endl;
+		int id2loc = locateElement(sqlist, it1+1, loc-1, tid2);		
+	//	std::cout<<"it1 = "<<it1<<std::endl;
+	//	std::cout<<"tid2 = "<<tid2<<std::endl;
+	//	std::cout<<"id2loc = "<<id2loc<<std::endl;
 		if(sqlist[id2loc].value==tid2){
+		//	std::cout<<"it1 = "<<it1<<std::endl;
+		//	std::cout<<"tid2 = "<<tid2<<std::endl;
+		//	std::cout<<"id2loc = "<<id2loc<<std::endl;
 			idx[0] = sqlist[it1].pos+1;
 			idx[1] = sqlist[id2loc].pos+1;
 			break;
@@ -171,37 +181,30 @@ std::vector<int> twoSum(std::vector<int> &numbers, int target) {
 		idx[0] = idx[1];
 		idx[1] = tmp;
 	}
+	
 	std::cout<<"idx[0] = "<<idx[0]<<", idx[1] = "<<idx[1]<<std::endl;
-	return idx;
+//	return idx;
+	
+	
 }
 
 #include<fstream>
 int main(){
 	
 	
-//	std::fstream input("in.txt",std::ios::in);
-//	std::vector<int> test(16023);
-//	int i = 1;
-//	while(i<=16023){
-//		input>>test[i-1];
-//		i++;
-//	}
-//	twoSum(test,16021);
+	std::fstream input("in.txt",std::ios::in);
+	std::vector<int> test(16023);
+	int i = 1;
+	while(i<=16023){
+		input>>test[i-1];
+		i++;
+	}
+	twoSum(test,16021);
 
-//	std::fstream input("in2.txt",std::ios::in);
-//	std::vector<int> test(282);
-//	int i = 1;
-//	while(i<=282){
-//		input>>test[i-1];
-//		i++;
-//	}
-//	twoSum(test,74);
 
-//	int myints[4] = {0,3,5,0};
-//	int myints[7] = {150,24,79,50,88,345,3};
-	int myints[9] = {3,2,1,-6,-3,8,88,345,6};
-	std::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
-	twoSum(fifth,-9);	
+//	int myints[4] = {0,4,3,1};
+//	std::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+//	twoSum(fifth,7);	
 
 	return 1;
 }
