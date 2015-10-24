5c991750a3c95ba12459b025ced58cbf0dc32ef9
Yuxi Pan <panyuxi@gmail.com>
2015-10-24 11:59:42 -0700

    test with eclipse

--- CloneGraph/CloneGraph.cxx
+++ CloneGraph/CloneGraph.cxx
@@ -19,7 +19,7 @@
 //                0 --- 2
 //                     / \
 //                     \_/
- 
+
 #include<iostream>
 #include<map>
 #include<vector>
@@ -30,75 +30,84 @@ class UndirectedGraphNode {
 public:
 	int label;
 	vector<UndirectedGraphNode*> neighbors;
-	UndirectedGraphNode(int x): label(x) {};
-};                
+	UndirectedGraphNode(int x) :
+			label(x) {
+	}
+	;
+};
 
 map<int, UndirectedGraphNode*> mymap; //store the pointer of already created nodes
 
-UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node){
-	
-	if(node==NULL) return NULL;
+UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+
+	if (node == NULL)
+		return NULL;
 
 	int label = node->label;
 	UndirectedGraphNode* new_node = NULL;
-	if(mymap[label]){
-		 return mymap[label]; //node already exists, return its location on the heap;
+	if (mymap[label]) {
+		return mymap[label]; //node already exists, return its location on the heap;
 	}
-		
-	else{
+
+	else {
 		new_node = new UndirectedGraphNode(label);
 		mymap[label] = new_node;
-		
+
 		//loop over its neighbors and create them
-		for(vector<UndirectedGraphNode*>::iterator it = node->neighbors.begin(); it != node->neighbors.end(); ++it){
+		for (vector<UndirectedGraphNode*>::iterator it =
+				node->neighbors.begin(); it != node->neighbors.end(); ++it) {
 			UndirectedGraphNode* i_neighbor = cloneGraph(*it);
 			(new_node->neighbors).push_back(i_neighbor);
 		}
 	}
-	
+
 	return new_node;
 }
 
-//Print the labels of node and its neghbors
-void PrintLabel(UndirectedGraphNode* node){
-	cout<<"node label: "<<node->label<<endl;
-	for(vector<UndirectedGraphNode*>::iterator it = node->neighbors.begin(); it != node->neighbors.end(); ++it){
-                cout<<"neghbor of label: "<<node->label<<", label = "<<(*it)->label<<endl;
-        }
+//Print the labels of node and its neighbors
+void PrintLabel(UndirectedGraphNode* node) {
+	cout << "node label: " << node->label << endl;
+	for (vector<UndirectedGraphNode*>::iterator it = node->neighbors.begin();
+			it != node->neighbors.end(); ++it) {
+		cout << "neghbor of label: " << node->label << ", label = "
+				<< (*it)->label << endl;
+	}
 }
 //test
-int main(){
-	
+int main() {
+
 	//create 0:
 	UndirectedGraphNode* node_0 = new UndirectedGraphNode(0);
-	
+
 	//create 1:
 	UndirectedGraphNode* node_1 = new UndirectedGraphNode(1);
-	
+
 	//create 2:
 	UndirectedGraphNode* node_2 = new UndirectedGraphNode(2);
-		
+
 	node_0->neighbors.push_back(node_1);
 	node_0->neighbors.push_back(node_2);
-	
+
 	node_1->neighbors.push_back(node_0);
 	node_1->neighbors.push_back(node_2);
-	
+
 	node_2->neighbors.push_back(node_0);
 	node_2->neighbors.push_back(node_1);
 	node_2->neighbors.push_back(node_2);
-	
-	cout<<"after construction"<<endl;
+
+	cout << "after construction" << endl;
 	UndirectedGraphNode* new_node = cloneGraph(node_0);
-	if(new_node==NULL){
-		cout<<"new_node is NULL"<<endl;
+	if (new_node == NULL) {
+		cout << "new_node is NULL" << endl;
 		return 0;
 	}
 	PrintLabel(new_node);
-	for(vector<UndirectedGraphNode*>::iterator it = new_node->neighbors.begin(); it != new_node->neighbors.end(); ++it){
+	for (vector<UndirectedGraphNode*>::iterator it =
+			new_node->neighbors.begin(); it != new_node->neighbors.end();
+			++it) {
 		PrintLabel(*it);
-	}	
-	
+	}
+
 	return 1;
 }
-                                   
+
